3b41da2e36cd23508ceba3461494f4d9cbbc8fa9
Jan Kelemen <jkeleme@gmail.com>
2018-11-13 19:47:42 +0100

    solution for second exercise

--- exercise/02/array.cpp
+++ exercise/02/array.cpp
@@ -1,1 +1,74 @@
 #include "array.h"
+
+#include <iterator>
+#include <stdexcept>
+
+namespace
+{
+  auto const size_check = [](int const& size) 
+  { 
+    if (size < 0) throw std::invalid_argument("invalid array size");
+    return size;
+  };
+
+  auto const bounds_check = [](int const& index, int const& upper_bound)
+  {
+    if(index < 0 || index >= upper_bound) throw std::out_of_range("invalid index");
+  };
+}
+
+array::array(int size) 
+  : p { new double[size_check(size)] }
+  , n { size }
+{
+}
+
+array::array(int size, double value) : array(size)
+{
+  std::fill(p, p + size, value);
+}
+
+array::array(array const& other) : array(other.n)
+{
+  std::copy(other.p, other.p + n, p);
+}
+
+array::array(array&& other) noexcept
+  : p { other.p }
+  , n { other.n }
+{
+  other.p = nullptr;
+  other.n = 0;
+}
+
+int array::size() const noexcept
+{
+    return n;
+}
+
+double& array::at(int index)
+{
+  bounds_check(index, size());
+  return p[index];
+}
+
+double const& array::at(int index) const
+{
+  bounds_check(index, size());
+  return p[index];
+}
+
+array& array::operator=(array other)
+{
+  std::swap(p, other.p);
+  std::swap(n, other.n);
+  return *this;
+}
+
+
+array::~array()
+{
+  delete[] p;
+  p = nullptr;
+  n = 0;
+}
--- exercise/02/array.h
+++ exercise/02/array.h
@@ -1,7 +1,30 @@
 // do not use standard container as member or base class
+
+#pragma once
+
+#include <type_traits>
+
 class array
 {
 	double* p;
 	int n;
 public:
+  explicit array(int size = 0);
+  array(int size, double value);
+  array(array const& other);
+  array(array&& other) noexcept;
+
+  int size() const noexcept;
+  double& at(int index);
+  double const& at(int index) const;
+
+  array& operator=(array other);
+
+  ~array();
 };
+
+static_assert(std::is_default_constructible_v<array>);
+static_assert(std::is_copy_constructible_v<array>);
+static_assert(std::is_copy_assignable_v<array>);
+static_assert(std::is_move_constructible_v<array>);
+static_assert(std::is_move_assignable_v<array>);
